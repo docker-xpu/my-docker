f5a6cdc3c319e3a4f6148a4917f85a19ae8bdb95
ahojcn <ahojcn@gmail.com>
2020-03-06 20:35:11 +0800

    [init] 初始化项目

---
+++ run.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"math/rand"
+	"mydocker/cgroups"
+	"mydocker/cgroups/subsystems"
+	"mydocker/container"
+	"mydocker/network"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func Run(tty bool, comArray []string, res *subsystems.ResourceConfig, containerName, volume, imageName string,
+	envSlice []string, nw string, portmapping []string) {
+	containerID := randStringBytes(10)
+	if containerName == "" {
+		containerName = containerID
+	}
+
+	parent, writePipe := container.NewParentProcess(tty, containerName, volume, imageName, envSlice)
+	if parent == nil {
+		log.Errorln("run.go NewParentProcess error!")
+		return
+	}
+
+	if err := parent.Start(); err != nil {
+		log.Errorln("run.go parent.Start() error:", err)
+	}
+
+	//record container info
+	containerName, err := recordContainerInfo(parent.Process.Pid, comArray, containerName, containerID, volume, portmapping)
+	if err != nil {
+		log.Errorln("run.go recordContainerInfo error:", err)
+		return
+	}
+
+	// use containerID as cgroup name
+	cgroupManager := cgroups.NewCgroupManager(containerID)
+	cgroupManager.Set(res)
+	cgroupManager.Apply(parent.Process.Pid)
+
+	if nw != "" {
+		// config container network
+		network.Init()
+		containerInfo := &container.ContainerInfo{
+			Id:          containerID,
+			Pid:         strconv.Itoa(parent.Process.Pid),
+			Name:        containerName,
+			PortMapping: portmapping,
+		}
+		log.Infoln("Run() containerInfo:", containerInfo)
+		log.Infoln("Run() portmapping:", portmapping)
+		if err := network.Connect(nw, containerInfo); err != nil {
+			log.Errorf("run.go Error Connect Network %v", err)
+			return
+		}
+	}
+
+	sendInitCommand(comArray, writePipe)
+
+	if tty {
+		parent.Wait()
+		deleteContainerInfo(containerName)
+		container.DeleteWorkSpace(volume, containerName)
+		cgroupManager.Destroy()
+	}
+
+}
+
+func sendInitCommand(comArray []string, writePipe *os.File) {
+	command := strings.Join(comArray, " ")
+	log.Infof("run.go sendInitCommand: command all is [%s]", command)
+	writePipe.WriteString(command)
+	writePipe.Close()
+}
+
+func recordContainerInfo(containerPID int, commandArray []string, containerName, id, volume string, portmapping []string) (string, error) {
+	createTime := time.Now().Format("2006-01-02 15:04:05")
+	command := strings.Join(commandArray, "")
+	containerInfo := &container.ContainerInfo{
+		Id:          id,
+		Pid:         strconv.Itoa(containerPID),
+		Command:     command,
+		CreatedTime: createTime,
+		Status:      container.RUNNING,
+		Name:        containerName,
+		Volume:      volume,
+		PortMapping: portmapping,
+	}
+
+	jsonBytes, err := json.Marshal(containerInfo)
+	log.Infoln("containerInfo:", string(jsonBytes))
+	if err != nil {
+		log.Errorf("run.go recordContainerInfo() error: %v", err)
+		return "", err
+	}
+	jsonStr := string(jsonBytes)
+
+	dirUrl := fmt.Sprintf(container.DefaultInfoLocation, containerName)
+	if err := os.MkdirAll(dirUrl, 0622); err != nil {
+		log.Errorf("run.go run.go MkdirAll %s error %v", dirUrl, err)
+		return "", err
+	}
+	fileName := dirUrl + "/" + container.ConfigName
+	file, err := os.Create(fileName)
+	defer file.Close()
+	if err != nil {
+		log.Errorf("run.go Create file %s error %v", fileName, err)
+		return "", err
+	}
+	if _, err := file.WriteString(jsonStr); err != nil {
+		log.Errorf("run.go File write string error %v", err)
+		return "", err
+	}
+
+	return containerName, nil
+}
+
+func deleteContainerInfo(containerId string) {
+	dirURL := fmt.Sprintf(container.DefaultInfoLocation, containerId)
+	if err := os.RemoveAll(dirURL); err != nil {
+		log.Errorf("run.go RemoveAll dir %s error: %v", dirURL, err)
+	}
+}
+
+func randStringBytes(n int) string {
+	letterBytes := "1234567890"
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
